f61bd811d2603bc0ebd520301063ee1638aa70ab
Abdul Diaz <diazabdulm@gmail.com>
2020-01-17 04:45:03 -0500

    added routes test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { mount } from "enzyme";
+import { Route } from "react-router-dom";
+import { shallow } from "enzyme";
 
 import App from "./App";
+import HomePage from "./pages/homepage/homepage.component";
 
 let wrapper;
 
 describe("<App />", () => {
   beforeEach(() => {
-    wrapper = mount(<App />);
+    wrapper = shallow(<App />);
   });
 
   it("renders App component", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders correct routes", () => {
+    const pathMap = wrapper.find(Route).reduce((pathMap, route) => {
+      const routeProps = route.props();
+      pathMap[routeProps.path] = routeProps.component;
+      return pathMap;
+    }, {});
+
+    expect(pathMap["/"]).toBe(HomePage);
   });
 });
